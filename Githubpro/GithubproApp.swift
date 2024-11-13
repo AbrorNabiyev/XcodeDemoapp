5d92c484ab40243439e5746073b012d1b3abc533
AbrorNabiyev <abrornabiyev695@gmail.com>
2024-11-13 12:17:21 +0300

    Initial Commit

---
+++ Githubpro/GithubproApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubproApp.swift
+//  Githubpro
+//
+//  Created by Abror on 13.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct GithubproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
